ba4a7e97ad672ffc2c9d55fce266d09b8dd8a791
hellojaliyah <jaliyahdharrison@gmail.com>
2023-10-05 02:05:48 -0400

    init

---
+++ scripts/deploy-nft.js
@@ -0,0 +1,35 @@
+const hre = require("hardhat");
+
+const contractAddress = "0xFD6794210Db002b3AB1ff6d15A2D0000E8d0C5F3";
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  // Deploy the Brownie Bakers Contract
+  const nftContract = await hre.ethers.deployContract("BrownieBakers", [contractAddress]);
+
+  // wait for the contract to deploy
+  await nftContract.waitForDeployment();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", nftContract.target);
+
+  // Sleep for 30 seconds while Etherscan indexes the new contract deployment
+  await sleep(30 * 1000); // 30s = 30 * 1000 milliseconds
+
+  // Verify the contract on etherscan
+  await hre.run("verify:verify", {
+    address: nftContract.target,
+    constructorArguments: [contractAddress],
+  });
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
